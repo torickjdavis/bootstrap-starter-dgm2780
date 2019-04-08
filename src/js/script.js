d80986b0a154075518b32f0c8fce1bd2be42ae70
Torick Davis <torickjdavis@gmail.com>
2019-04-08 01:52:46 +0000

    home page design

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // Custom JS here
+const mediumBreakPoint = 768; // bootstrap md break point
+
+document.addEventListener("DOMContentLoaded", () => {
+  let isPhone = (window.innerWidth < mediumBreakPoint);
+  if (!isPhone) {
+    let backgroundIconImages = document.querySelectorAll(".bg-icon-image");
+    for (let icon of backgroundIconImages) {
+      let url = icon.getAttribute("data-image");
+      if (url) {
+        icon.style.backgroundImage = `url(${url})`;
+      }
+    }
+  }
+});
